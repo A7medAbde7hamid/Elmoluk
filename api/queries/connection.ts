23b4bf26e715329da4987340b7d04ca282caf105
A7med <143652644+A7medAbde7hamid@users.noreply.github.com>
2026-07-02 20:25:28 +0300

    Add DATABASE_CA env for SSL MySQL connection

--- api/queries/connection.ts
+++ api/queries/connection.ts
@@ -1,16 +1,34 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import { URL } from "url";
 import { env } from "../lib/env.js";
 import * as schema from "@db/schema";
 import * as relations from "@db/relations";
 
 const fullSchema = { ...schema, ...relations };
 
+function createPool() {
+  const opts: mysql.PoolOptions = { uri: env.databaseUrl, waitForConnections: true, connectionLimit: 5 };
+
+  if (env.databaseCa) {
+    opts.ssl = { ca: env.databaseCa };
+  }
+
+  const parsed = new URL(env.databaseUrl);
+  if (parsed.searchParams.has("ssl")) {
+    try {
+      opts.ssl = { ...opts.ssl, ...JSON.parse(parsed.searchParams.get("ssl")!) };
+    } catch { /* ignore invalid JSON */ }
+  }
+
+  return mysql.createPool(opts);
+}
+
 let instance: ReturnType<typeof drizzle<typeof fullSchema>>;
 
 export function getDb() {
   if (!instance) {
-    const pool = mysql.createPool(env.databaseUrl);
+    const pool = createPool();
     instance = drizzle(pool as any, { mode: 'default', schema: fullSchema });
   }
   return instance;
